70942f072a91cf5c33d922410e3802d810b8b11f
learninglizard1 <SachinSinghBade@myop.ac.nz>
2026-04-16 09:41:12 +1200

    update

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -7,6 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
@@ -21,17 +23,26 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			
 	}
 	
-	public void update() {
+	private void update() {
 		
+	}
+	
+	private void paintDottedLine(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g.create();
+		Stroke dashed = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{9}, 0);
+		g2d.setStroke(dashed);
+		g2d.setPaint(Color.WHITE);
+		g2d.drawLine(getWidth() / 2, 0, getWidth() /2, getHeight());
+		g2d.dispose();
 	}
 	
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.WHITE);
-		g.fillRect(20, 20, 100, 100);
+		paintDottedLine(g);
 	}
+	
 	
 	@Override
 	public void keyTyped(KeyEvent event) {
@@ -53,7 +64,9 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		// TODO Auto-generated method stub
+		
+		update();
+		repaint();
 		
 	}
 	
